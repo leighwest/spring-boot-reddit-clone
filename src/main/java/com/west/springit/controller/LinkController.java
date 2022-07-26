35529bca135ac4199e070fa533a46fa9c298a86b
Leigh West <leighwest@hotmail.com>
2022-07-26 22:43:36 +0000

    Added Link Controller

---
+++ src/main/java/com/west/springit/controller/LinkController.java
@@ -0,0 +1,46 @@
+package com.west.springit.controller;
+
+import com.west.springit.domain.Link;
+import com.west.springit.repository.LinkRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkController {
+
+    private final LinkRepository linkRepository;
+
+    // as of spring 4.3, if there is a single constructor, spring will look at the arguments and wire them up for us - there's no need for the @autowired annotation anymore
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    @GetMapping("/")
+    public List<Link> list() {
+        return linkRepository.findAll();
+    }
+
+    // CRUD
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable Long id) {
+        return linkRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Link update(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        linkRepository.deleteById(id);
+    }
+}
