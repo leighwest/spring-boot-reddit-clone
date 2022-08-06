c70a8794047f2ac4da2dedd3bbfbaa2a3a388ca7
Leigh West <leighwest@hotmail.com>
2022-08-06 09:21:14 +0000

    Added Thymeleaf templates, css styling

--- src/main/java/com/west/springit/controller/HomeController.java
+++ src/main/java/com/west/springit/controller/HomeController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
-
 // here we're using controller instead of rest controller because the controller annotation will tell spring to look in
 // resources/templates and return the specific template.
 @Controller
@@ -16,9 +13,9 @@ public class HomeController {
 //    public String home() {
 //        return "index";
 //    }
-    @GetMapping("/")
-    public String home(Model model, HttpServletRequest request) {
-        model.addAttribute("message", "Hello World!");
-        return "index";
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Hello, Thymeleaf!");
+        return "/link/index";
     }
 }
