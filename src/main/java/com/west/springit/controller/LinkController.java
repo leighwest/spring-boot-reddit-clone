dbe85c97b3a2ca5a4b1a9baa3c3817116e7b3496
Leigh West <leighwest@hotmail.com>
2022-08-11 08:53:27 +0000

    Added link page, hooked up LinkController

--- src/main/java/com/west/springit/controller/LinkController.java
+++ src/main/java/com/west/springit/controller/LinkController.java
@@ -1,10 +1,13 @@
 package com.west.springit.controller;
 
 
+import com.west.springit.domain.Link;
 import com.west.springit.repository.LinkRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class LinkController {
@@ -19,5 +22,17 @@ public class LinkController {
     public String list(Model model) {
         model.addAttribute("links", linkRepository.findAll());
         return "link/list";
+    }
+
+    @GetMapping("/link/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        Optional<Link> link = linkRepository.findById(id);
+        if(link.isPresent()) {
+            model.addAttribute("link",link.get());
+            model.addAttribute("success", model.containsAttribute("success"));
+            return "link/view";
+        } else {
+            return "redirect:/";
+        }
     }
 }
