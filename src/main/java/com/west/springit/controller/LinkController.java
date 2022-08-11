5d16809a8553c15f28570de2aaff39deeae5a59c
Leigh West <leighwest@hotmail.com>
2022-08-11 09:31:26 +0000

    Added validation constraints to Link domain, added ability to submit link

--- src/main/java/com/west/springit/controller/LinkController.java
+++ src/main/java/com/west/springit/controller/LinkController.java
@@ -3,14 +3,21 @@ package com.west.springit.controller;
 
 import com.west.springit.domain.Link;
 import com.west.springit.repository.LinkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
 public class LinkController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 
     private LinkRepository linkRepository;
 
@@ -33,6 +40,28 @@ public class LinkController {
             return "link/view";
         } else {
             return "redirect:/";
+        }
+    }
+
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link",new Link());
+        return "link/submit";
+    }
+
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            logger.info("Validation errors were found while submitting a new link.");
+            model.addAttribute("link", link);
+            return "link/submit";
+        } else {
+            linkRepository.save(link);
+            logger.info("New link was saved successfully");
+            redirectAttributes
+                    .addAttribute("id", link.getId())
+                    .addFlashAttribute("success", true);
+            return "redirect:/link/{id}";
         }
     }
 }
