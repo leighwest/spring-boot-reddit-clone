b555c9697629fd919bc521e891f4020237c7912c
Leigh West <leighwest@hotmail.com>
2022-08-27 01:19:17 +0000

    Added user repository and UserDetailsServiceImpl

--- src/main/java/com/west/springit/security/SecurityConfiguration.java
+++ src/main/java/com/west/springit/security/SecurityConfiguration.java
@@ -3,13 +3,19 @@ package com.west.springit.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfiguration  {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,6 +26,11 @@ public class SecurityConfiguration  {
             .and()
             .formLogin();
         return http.build();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+
     }
 
 }
---
+++ src/main/java/com/west/springit/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.west.springit.security;
+
+import com.west.springit.domain.User;
+import com.west.springit.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException(email);
+        }
+        return user.get();
+    }
+}
